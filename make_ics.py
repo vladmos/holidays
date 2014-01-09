1cc3b467b407adaf351541dc594f8bc3eb89e94b
Vladimir Moskva <vladmos@yandex-team.ru>
2014-01-09 16:12:54 +0400

    Feed for Ukraine

--- make_ics.py
+++ make_ics.py
@@ -6,28 +6,35 @@ from cStringIO import StringIO
 from hashlib import md5
 
 COUNTRIES = {
-    'ru': 225,
+    'ru': {
+        'id': 225,
+        'header': 'Праздники России',
+    },
+    'ua': {
+        'id': 187,
+        'header': 'Праздники Украины',
+    }
 }
 CALENDAR_URL = 'http://calendar.yandex.ru/export/holidays.xml?start_date=2014-01-01&end_date=%(year)s-12-31&' +\
                'country_id=%(country)s&out_mode=overrides'
 
 
-def fetch_feed():
+def fetch_feed(country_code):
     year = datetime.date.today().year
-    url = CALENDAR_URL % {'year': year + 1, 'country': COUNTRIES['ru']}
+    url = CALENDAR_URL % {'year': year + 1, 'country': country_code}
     xml = requests.get(url).text.encode('utf-8')
     tree = ElementTree.fromstring(xml)
     days = tree.find('get-holidays').find('days').findall('day')
     return days
 
 
-def make_ics(feed):
+def make_ics(feed, header):
     io = StringIO()
     io.write('BEGIN:VCALENDAR\n')
     io.write('CALSCALE:GREGORIAN\n')
     io.write('PRODID:fulc.ru\n')
     io.write('VERSION:2.0\n')
-    io.write('X-WR-CALNAME:Праздники России\n')
+    io.write('X-WR-CALNAME:%s\n' % header)
 
     for day in feed:
         attributes = day.attrib
@@ -54,10 +61,11 @@ def make_ics(feed):
 
 
 def main():
-    feed = fetch_feed()
-    ics = make_ics(feed)
-    with open('ru_holidays.ics', 'w') as f:
-        f.write(ics)
+    for country_code, country_data in COUNTRIES.iteritems():
+        feed = fetch_feed(country_data['id'])
+        ics = make_ics(feed, country_data['header'])
+        with open('%s_holidays.ics' % country_code, 'w') as f:
+            f.write(ics)
 
 
 if __name__ == '__main__':
